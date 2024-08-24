557ade9952aff2dea00424e37f65ba1eb5584c95
김건 <kimgun@gimgeons-MacBook-Air.local>
2024-08-24 16:57:44 +0900

    UVR 에러 핸들링, 추론 편의성 개선작업

--- src/execute.py
+++ src/execute.py
@@ -1,6 +1,10 @@
 import os
 import sys
 import re
+import json
+import sox
+
+from main import voice_change, find_full_path
 
 
 # 파일 경로에서 1_,2_ 를 추출하는 함수
@@ -12,40 +16,48 @@ def extract_number(file_path):
     return int(match.group(1)) if match else float("inf")
 
 
-from main import voice_change, find_full_path
+def change_pitch_sox(input_filepath, output_filepath, semitones):
+    tfm = sox.Transformer()
+    tfm.pitch(semitones)
+    tfm.build(input_filepath, output_filepath)
+
+
+def process_mp3_files(input_directory, output_directory, semitones):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for filename in os.listdir(input_directory):
+        if filename.endswith("_mr.mp3"):
+            input_filepath = os.path.join(input_directory, filename)
+            output_filepath = os.path.join(output_directory, filename)
+            change_pitch_sox(input_filepath, output_filepath, semitones)
+            print(f"{filename}의 피치가 {semitones} 반음만큼 변경되었습니다.")
+
 
 if __name__ == "__main__":
-    input_song_titles_str = sys.argv[1]  # 첫 번째 인자
-    input_song_titles = input_song_titles_str.split(",")
-    voice_model = sys.argv[2]  # 두 번째 인자
-    isMan = sys.argv[3].lower() == "true"  # 세 번째 인자
-    pitch_values_str = sys.argv[4]
-    pitch_values = list(map(int, pitch_values_str.split(",")))
-    pitch_control = sys.argv[5].lower() == "true"
-    print(
-        "Received arguments",
-        input_song_titles,
-        voice_model,
-        isMan,
-        pitch_values,
-        pitch_control,
-    )
+    song_datas_str = sys.argv[1]  # 첫 번째 인자
+    song_datas = json.loads(song_datas_str)
+    print(song_datas)
 
-for song_title in input_song_titles:
+for song_data in song_datas:
+    song_title = song_data["song_title"]
+    voice_model = song_data["voice_model"]
+    pitch_value = song_data["pitch_value"]
+    isMan = song_data["isMan"]
+
     input_paths = find_full_path(song_title, isMan)
     # 파일 경로를 숫자에 따라 정렬
     sorted_input_paths = sorted(input_paths, key=extract_number)
-    if not os.path.exists(f"/content/drive/MyDrive/infer/{song_title}"):
-        os.mkdir(f"/content/drive/MyDrive/infer/{song_title}")
-    for index, input_path in enumerate(sorted_input_paths):
-        if pitch_control:
-            pitch_value = pitch_values[index]
-            print(pitch_value)
-        else:
-            pitch_value = 0
+    # 폴더 생성
+    ## 보이스 모델 폴더
+    if not os.path.exists(f"/content/drive/MyDrive/infer/{voice_model}"):
+        os.mkdir(f"/content/drive/MyDrive/infer/{voice_model}")
+    ## 추론 폴더
+    os.mkdir(f"/content/drive/MyDrive/infer/{voice_model}/[{pitch_value}]{song_title}")
+
+    for input_path in sorted_input_paths:
         file_name = os.path.basename(input_path)
-        output_path = f"/content/drive/MyDrive/infer/{song_title}/{file_name}.mp3"
-        print("곡", input_path, "피치 변경값", pitch_value, type(pitch_value))
+        output_path = f"/content/drive/MyDrive/infer/{voice_model}/[{pitch_value}]{song_title}/{file_name}.mp3"
         voice_change(
             voice_model,
             input_path,
@@ -59,3 +71,37 @@ for song_title in input_song_titles:
             crepe_hop_length=128,
             is_webui=0,
         )
+        # 원키가 아닐경우, mr 처리
+        if pitch_value != 0:
+            mr_input_path = os.path.dirname(input_path)
+            mr_output_path = f"/content/drive/MyDrive/infer/{voice_model}/[{pitch_value}]{song_title}/mr"
+            process_mp3_files(mr_input_path, mr_output_path, pitch_value)
+
+
+# for song_title in input_song_titles:
+#     input_paths = find_full_path(song_title, isMan)
+#     # 파일 경로를 숫자에 따라 정렬
+#     sorted_input_paths = sorted(input_paths, key=extract_number)
+#     if not os.path.exists(f"/content/drive/MyDrive/infer/{song_title}"):
+#         os.mkdir(f"/content/drive/MyDrive/infer/{song_title}")
+#     for index, input_path in enumerate(sorted_input_paths):
+#         if pitch_control:
+#             pitch_value = pitch_values[index]
+#             print(pitch_value)
+#         else:
+#             pitch_value = 0
+#         file_name = os.path.basename(input_path)
+#         output_path = f"/content/drive/MyDrive/infer/{song_title}/{file_name}.mp3"
+#         voice_change(
+#             voice_model,
+#             input_path,
+#             output_path,
+#             pitch_value,
+#             f0_method="rmvpe",
+#             index_rate=0.66,
+#             filter_radius=3,
+#             rms_mix_rate=0.25,
+#             protect=0.33,
+#             crepe_hop_length=128,
+#             is_webui=0,
+#         )
